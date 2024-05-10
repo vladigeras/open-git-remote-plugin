dcea0a5bcb66390aad3639a52b3115080af7306d
Vladislav Gerasimov <vladi6eras@gmail.com>
2024-05-11 00:21:06 +0300

    Refactoring

--- src/main/kotlin/com/vladigeras/opengitremote/OpenBrowserAction.kt
+++ src/main/kotlin/com/vladigeras/opengitremote/OpenBrowserAction.kt
@@ -17,14 +17,14 @@ class OpenBrowserAction : AnAction() {
             notifier.showNotificationAboutNoActiveProject(e.project)
             return
         }
-        val repositories = e.project!!.let { GitRepositoryManager.getInstance(it).repositories }
-        if (repositories.isEmpty()) {
-            LOG.info("Git repository was not found")
-            notifier.showNotificationAboutNoGitRepo(e.project!!)
-            return
-        }
+        val repository = e.project!!.let { GitRepositoryManager.getInstance(it).repositories }.firstOrNull()
+            ?: run {
+                LOG.info("Git repository was not found")
+                notifier.showNotificationAboutNoGitRepo(e.project!!)
+                return
+            }
 
-        val remotes = repositories.flatMap { it.remotes }
+        val remotes = repository.remotes
             .map { Pair(it.name, it.urls.firstOrNull()) }
             .filterNot { it.second == null }
 
