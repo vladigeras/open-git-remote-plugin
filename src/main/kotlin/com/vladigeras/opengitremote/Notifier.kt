e652990b1505b5d26ca32c38ea267f12d1bac2c2
Vladislav Gerasimov <vladi6eras@gmail.com>
2024-05-10 21:32:26 +0300

    Notification with icon

--- src/main/kotlin/com/vladigeras/opengitremote/Notifier.kt
+++ src/main/kotlin/com/vladigeras/opengitremote/Notifier.kt
@@ -3,6 +3,7 @@ package com.vladigeras.opengitremote
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType.INFORMATION
 import com.intellij.openapi.project.Project
+import com.vladigeras.opengitremote.Icons.TOOL
 
 class Notifier {
     private val notificationGroup = NotificationGroupManager.getInstance()
@@ -10,9 +11,11 @@ class Notifier {
 
     fun showNotificationAboutNoActiveProject(project: Project?) = notificationGroup
         .createNotification("Active project is absent", "Open the project first", INFORMATION)
+        .apply { icon = TOOL }
         .notify(project)
 
     fun showNotificationAboutEmptyRemotes(project: Project) = notificationGroup
         .createNotification("Remotes are empty", "Add remotes first", INFORMATION)
+        .apply { icon = TOOL }
         .notify(project)
 }
